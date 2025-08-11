3ddfa25e91a1eb42dc87b4851ace8c31668f5fbd
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-08-11 09:17:36 +0800

    修复路径问题

--- SourceCode/CabbageEditorBackend/utils/StaticComponents.py
+++ SourceCode/CabbageEditorBackend/utils/StaticComponents.py
@@ -2,10 +2,11 @@ from logging import root
 import os
 from PyQt6.QtCore import QUrl
 
-root_dir = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+root_dir = os.path.split(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0])[0]
 html_path = os.path.join(root_dir, "CabbageEditorFrontend", "index.html")
 obj_dir = os.path.join(root_dir, "TestCase", "AddModelTest")
 url = QUrl.fromLocalFile(html_path)
+print(html_path)
 
 try:
     import CabbageEngine
